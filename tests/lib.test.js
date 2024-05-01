0cce7911e5c797d2be80267a5e5bea35ebef2be1
Nicholas Gregory <nicholas.babcock092@gmail.com>
2024-05-01 13:41:11 -0700

    Class: Effect (getDamage and setDamage);

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -2,6 +2,21 @@ import { describe, it } from "node:test";
 
 import Timeline from "../lib/Timeline.js";
 import * as assert from "node:assert";
+import Effect from "../lib/Effect.js";
+
+describe('Effect tests', () => {
+    const effect = new Effect('Deal 1 damage to an independent target');
+
+    it('reports damage', () => {
+        assert.strictEqual(effect.getDamage(), 1);
+    });
+
+    it('sets damage', () => {
+        effect.setDamage(2);
+
+        assert.strictEqual(effect.getDamage(), 2);
+    });
+});
 
 describe('Timeline tests', () => {
     it('resolves correctly', () => {
