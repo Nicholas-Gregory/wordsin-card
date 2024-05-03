23f52070a45dea62aeffb87c6c3ae54ac7b7f2a5
Nicholas Gregory <nicholas.babcock092@gmail.com>
2024-05-03 12:57:17 -0700

    getters for effects

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -5,120 +5,61 @@ import * as assert from "node:assert";
 import Effect from "../lib/Effect.js";
 
 describe('Effect tests', () => {
-    const damageEffect = new Effect('Deal 1 damage to an independent target');
-    const healEffect = new Effect('Independent target heals for 5');
-    const increaseDamageEffect = new Effect("Increase independent target offensive effect's damage by 1");
-    const decreaseDamageEffect = new Effect("Decrease independent target offensive effect's damage by 1");
-
-    it('reports damage', () => {
-        assert.strictEqual(damageEffect.getDamage(), 1);
+    it('reports amount correctly', () => {
+        const damageEffect = new Effect('Deal 5');
+        const statIncreaseEffect = new Effect('Increase wellness 3');
+        const statDecreaseEffect = new Effect('Decrease wellness 1');
+        const setStatEffect = new Effect('Set wellness 10');
+        const drawEffect = new Effect('Draw 2');
+        const healEffect = new Effect('Heal 5');
+        const increaseDamageEffect = new Effect('Increase damage 1');
+        const decreaseDamageEffect = new Effect('Decrease damage 1');
+        const increaseHealEffect = new Effect('Increase heal 1');
+        const decreaseHealEffect = new Effect('Decrease heal 1');
+        const setDamageEffect = new Effect('Set damage to 0');
+        const setHealEffect = new Effect('Set heal to 10');
+        
+        assert.strictEqual(damageEffect.getAmount(), 5);
+        assert.strictEqual(statIncreaseEffect.getAmount(), 3);
+        assert.strictEqual(statDecreaseEffect.getAmount(), -1);
+        assert.strictEqual(setStatEffect.getAmount(), 10);
+        assert.strictEqual(drawEffect.getAmount(), 2);
+        assert.strictEqual(healEffect.getAmount(), 5);
+        assert.strictEqual(increaseDamageEffect.getAmount(), 1);
+        assert.strictEqual(decreaseDamageEffect.getAmount(), -1);
+        assert.strictEqual(increaseHealEffect.getAmount(), 1);
+        assert.strictEqual(decreaseHealEffect.getAmount(), -1);
+        assert.strictEqual(setDamageEffect.getAmount(), 0);
+        assert.strictEqual(setHealEffect.getAmount(), 10);
     });
 
-    it('sets damage', () => {
-        damageEffect.setDamage(2);
+    it('reports words correctly', () => {
+        const oneWordEffect = new Effect('Apply burn');
+        const twoWordsEffect = new Effect('Apply wet, cold');
 
-        assert.strictEqual(damageEffect.getDamage(), 2);
+        assert.deepStrictEqual(oneWordEffect.getWords(), ['burn']);
+        assert.deepStrictEqual(twoWordsEffect.getWords(), ['wet', 'cold']);
     });
 
-    it('reports heal amount', () => {
-        assert.strictEqual(healEffect.getHealAmount(), 5);
+    it('reports stats correctly', () => {
+        const increaseStatEffect = new Effect('Increase wellness 1');
+        const decreaseStatEffect = new Effect('Decrease agility 2');
+        const setStatAmount = new Effect('Set swiftness 5');
+
+        assert.strictEqual(increaseStatEffect.getStat(), 'wellness');
+        assert.strictEqual(decreaseStatEffect.getStat(), 'agility');
+        assert.strictEqual(setStatAmount.getStat(), 'swiftness');
     });
 
-    it('sets heal amount', () => {
-        healEffect.setHealAmount(2);
+    it('reports effects correctly', () => {
+        const oneEffectInsertEffect = new Effect('Insert "Deal 3"');
+        const twoEffectsInsertEffect = new Effect('Insert "Deal 2", "Heal 1"');
+        const oneEffectChangeEffect = new Effect('Change to "Apply burn"');
+        const twoEffectsChangeEffect = new Effect('Change to "Apply wet", "Apply cold"');
 
-        assert.strictEqual(healEffect.getHealAmount(), 2);
-    });
-
-    it('reports damage modifier', () => {
-        assert.strictEqual(increaseDamageEffect.getDamageModifier(), 1);
-        assert.strictEqual(decreaseDamageEffect.getDamageModifier(), -1);
-    });
-
-    it('sets damage modifier', () => {
-        increaseDamageEffect.setDamageModifier(2);
-
-        assert.strictEqual(increaseDamageEffect.getDamageModifier(), 2);
+        assert.deepStrictEqual(oneEffectInsertEffect.getEffects(), ['Deal 3']);
+        assert.deepStrictEqual(twoEffectsInsertEffect.getEffects(), ['Deal 2', 'Heal 1']);
+        assert.deepStrictEqual(oneEffectChangeEffect.getEffects(), ['Apply burn']);
+        assert.deepStrictEqual(twoEffectsChangeEffect.getEffects(), ['Apply wet', 'Apply cold']);
     })
 });
-
-describe('Timeline tests', () => {
-    it('resolves correctly', () => {
-        const timeline = new Timeline([
-            [
-                new Effect('Deal 2 damage to an independent target', 'Mr. Robot', 'Elliot Alderson', 0)
-            ], 
-            [
-                new Effect("Deal 3 damage to the card's target", 'Elliot Alderson', 'Mr.Robot')       
-            ],
-            [
-                new Effect("Increase independent target offensive effect's damage by 3", 'Mr. Robot', 0)
-            ],
-            [
-                new Effect("Increase independent target defensive effect's heal amount by 2", 'Mr. Robot', 1)
-            ],
-            [
-                new Effect("Independent target heals for 5", 'Mr. Robot', 'Mr. Robot', 1)
-            ],
-            [
-                new Effect("Decrease independent target character's awareness by 1", 'Elliot Alderson', 'Mr. Robot')
-            ],
-            [
-                new Effect("Decrease independent target character's power by 1", 'Mr. Robot', 'Elliot Alderson')
-            ]
-        ]);
-
-        assert.deepStrictEqual(timeline.resolve({
-            characters: [
-                {
-                    id: 'Mr. Robot',
-                    equipment: [
-                        {
-                            bonuses: [
-                                {
-                                    text: 'Increase all damage in Offensive Effects by 2'
-                                }
-                            ]
-                        }
-                    ],
-                    stats: {
-                        power: 3,
-                        wellness: 2,
-                        awareness: 2
-                    } 
-                },
-                {
-                    id: 'Elliot Alderson',
-                    equipment: [],
-                    stats: {
-                        power: 2,
-                        wellness: 1,
-                        awareness: 4
-                    }
-                }
-            ]
-        }), [
-            [
-                new Effect('Deal 10 damage to an independent target', 'Mr. Robot', 'Elliot Alderson', 0)
-            ],
-            [
-                new Effect("Deal 5 damage to the card's target", 'Elliot Alderson', 'Mr.Robot')
-            ],
-            [
-                new Effect("Increase independent target offensive effect's damage by 3", 'Mr. Robot', 0)
-            ],
-            [
-                new Effect("Increase independent target defensive effect's heal amount by 2", 'Mr. Robot', 1)
-            ],
-            [
-                new Effect("Independent target heals for 9", 'Mr. Robot', 'Mr. Robot', 1)
-            ],
-            [
-                new Effect("Decrease independent target character's awareness by 5", 'Elliot Alderson', 'Mr. Robot')
-            ],
-            [
-                new Effect("Decrease independent target character's power by -2", 'Mr. Robot', 'Elliot Alderson')
-            ]
-        ])
-    })
-})
