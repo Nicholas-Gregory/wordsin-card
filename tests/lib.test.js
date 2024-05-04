5bcb09848e823fbc5f0828c5f8678858939782db
Nicholas Gregory <nicholas.babcock092@gmail.com>
2024-05-03 20:11:25 -0700

    Improvement: Class: Effect (parsing target class);

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -6,18 +6,18 @@ import Effect from "../lib/Effect.js";
 
 describe('Effect tests', () => {
     it('reports amount correctly', () => {
-        const damageEffect = new Effect('Deal 5 to an independent target');
-        const statIncreaseEffect = new Effect('Increase wellness 3 for an independent target');
-        const statDecreaseEffect = new Effect('Decrease wellness 1 for an independent target');
-        const setStatEffect = new Effect('Set wellness 10 for an independent target');
-        const drawEffect = new Effect('Draw 2 for an independent target');
-        const healEffect = new Effect('Heal 5 for an independent target');
-        const increaseDamageEffect = new Effect('Increase damage 1 for an independent target');
-        const decreaseDamageEffect = new Effect('Decrease damage 1 for an independent target');
-        const increaseHealEffect = new Effect('Increase heal 1 for an independent target');
-        const decreaseHealEffect = new Effect('Decrease heal 1 for an independent target');
-        const setDamageEffect = new Effect('Set damage to 0 for an independent target');
-        const setHealEffect = new Effect('Set heal to 10 for an independent target');
+        const damageEffect = new Effect('Deal 5 to an independent target character');
+        const statIncreaseEffect = new Effect('Increase wellness 3 for an independent target character');
+        const statDecreaseEffect = new Effect('Decrease wellness 1 for an independent target character');
+        const setStatEffect = new Effect('Set wellness 10 for an independent target character');
+        const drawEffect = new Effect('Draw 2 for an independent target character');
+        const healEffect = new Effect('Heal 5 for an independent target character');
+        const increaseDamageEffect = new Effect('Increase damage 1 for an independent target character');
+        const decreaseDamageEffect = new Effect('Decrease damage 1 for an independent target character');
+        const increaseHealEffect = new Effect('Increase heal 1 for an independent target character');
+        const decreaseHealEffect = new Effect('Decrease heal 1 for an independent target character');
+        const setDamageEffect = new Effect('Set damage to 0 for an independent target character');
+        const setHealEffect = new Effect('Set heal to 10 for an independent target character');
         
         assert.strictEqual(damageEffect.getAmount(), 5);
         assert.strictEqual(statIncreaseEffect.getAmount(), 3);
@@ -34,17 +34,17 @@ describe('Effect tests', () => {
     });
 
     it('reports words correctly', () => {
-        const oneWordEffect = new Effect('Apply burn to an independent target');
-        const twoWordsEffect = new Effect('Apply wet, cold to an independent target');
+        const oneWordEffect = new Effect('Apply burn to an independent target character');
+        const twoWordsEffect = new Effect('Apply wet, cold to an independent target character');
 
         assert.deepStrictEqual(oneWordEffect.getWords(), ['burn']);
         assert.deepStrictEqual(twoWordsEffect.getWords(), ['wet', 'cold']);
     });
 
     it('reports stats correctly', () => {
-        const increaseStatEffect = new Effect('Increase wellness by 1 for an independent target');
-        const decreaseStatEffect = new Effect('Decrease agility by 2 for an independent target');
-        const setStatAmount = new Effect('Set swiftness to 5 for an independent target');
+        const increaseStatEffect = new Effect('Increase wellness by 1 for an independent target character');
+        const decreaseStatEffect = new Effect('Decrease agility by 2 for an independent target character');
+        const setStatAmount = new Effect('Set swiftness to 5 for an independent target character');
 
         assert.strictEqual(increaseStatEffect.getStat(), 'wellness');
         assert.strictEqual(decreaseStatEffect.getStat(), 'agility');
@@ -52,10 +52,10 @@ describe('Effect tests', () => {
     });
 
     it('reports effects correctly', () => {
-        const oneEffectInsertEffect = new Effect('Insert "Deal 3" in an independent target');
-        const twoEffectsInsertEffect = new Effect('Insert "Deal 2", "Heal 1" in an independent target');
-        const oneEffectChangeEffect = new Effect('Change text to "Apply burn" on an independent target');
-        const twoEffectsChangeEffect = new Effect('Change text to "Apply wet", "Apply cold" on an independent target');
+        const oneEffectInsertEffect = new Effect('Insert "Deal 3" in an independent target character');
+        const twoEffectsInsertEffect = new Effect('Insert "Deal 2", "Heal 1" in an independent target character');
+        const oneEffectChangeEffect = new Effect('Change text to "Apply burn" on an independent target character');
+        const twoEffectsChangeEffect = new Effect('Change text to "Apply wet", "Apply cold" on an independent target character');
 
         assert.deepStrictEqual(oneEffectInsertEffect.getEffects(), ['Deal 3']);
         assert.deepStrictEqual(twoEffectsInsertEffect.getEffects(), ['Deal 2', 'Heal 1']);
@@ -64,10 +64,22 @@ describe('Effect tests', () => {
     });
 
     it('reports target type correctly', () => {
-        const independentTarget = new Effect('Deal 2 to independent target');
-        const cardsTarget = new Effect("Deal 2 to card's target");
+        const independentTarget = new Effect('Deal 2 to independent target character');
+        const cardsTarget = new Effect("Deal 2 to card's target character");
 
         assert.strictEqual(independentTarget.targetType, 'independent target');
         assert.strictEqual(cardsTarget.targetType, "card's target");
+    });
+
+    it('reports target class correctly', () => {
+        assert.strictEqual(new Effect('Deal 2 to independent target character').targetClass, 'character');
+        assert.strictEqual(new Effect('Deal 2 to independent target enemy').targetClass, 'enemy');
+        assert.strictEqual(new Effect('Deal 2 to independent target ally').targetClass, 'ally');
+        assert.strictEqual(new Effect('Deal 2 to independent target object').targetClass, 'object');
+        assert.strictEqual(new Effect('Insert "Apply burn" in independent target spell').targetClass, 'spell');
+        assert.strictEqual(new Effect('Insert "Apply burn" in independent target offensive effect').targetClass, 'offensive effect');
+        assert.strictEqual(new Effect('Insert "Apply burn" in independent target defensive effect').targetClass, 'defensive effect');
+        assert.strictEqual(new Effect('Insert "Apply burn" in independent target utility effect').targetClass, 'utility effect');
+        assert.strictEqual(new Effect('Insert "Apply burn" in independent target card').targetClass, 'card');
     })
 });
