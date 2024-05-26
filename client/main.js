df42d65117050c7faf820ec0c0e9a26d37a68326
Nicholas Gregory <nicholas.babcock092@gmail.com>
2024-05-25 20:22:15 -0700

    Class: DialogueSystem;

--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
 import { Application, Assets, Graphics, Sprite, Texture } from 'pixi.js';
-import ShadowBoxRenderer from './lib/renderers/ShadowBoxRenderer';
-import DialogueManager from './lib/managers/DialogueManager';
+
 
 (async () =>
 {
@@ -14,10 +13,5 @@ import DialogueManager from './lib/managers/DialogueManager';
 
     document.body.appendChild(app.canvas);
 
-    const dialogueBox = new DialogueManager(app);
-    dialogueBox
-    .initBox()
-    .init();
-
-    app.stage.addChild(dialogueBox);
+    
 ;})();
